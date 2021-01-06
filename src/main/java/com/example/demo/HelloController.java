2463e81406dc4a40e297fe84e6116159649d6da2
vinodkpk@gmail.com <Octtue@377>
2021-01-05 22:34:01 -0500

    First update

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/James")
+    public String index1() {
+        return "Greetings James from Spring Boot!";
+    }
+
+}
