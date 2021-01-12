81be3e3b20ba9c3553b4e5e32ceb46f1b6484dc1
vinodkpk@gmail.com <Octtue@377>
2021-01-12 16:23:07 -0500

    WIth Eureka

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,23 +1,21 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
 public class HelloController {
-    @RequestMapping("home")
+    @RequestMapping("/home")
     public String home(HttpServletRequest req){
 
         String name=req.getParameter("name");
         System.out.println("Hi"+name);
        HttpSession session=req.getSession();
        session.setAttribute("name",name);
-        return "home.jsp";
+        return "home";
 
     }
 
---
+++ src/main/resources/META-INF/resources/home.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+    <head></head>
+    <body>
+        Hello User....
+    </body>
+</html>
